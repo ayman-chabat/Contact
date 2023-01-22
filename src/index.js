42056094e81af8f8b29e53e0c34b41d24dbe3e0a
Ayman Chabat <aymanechabat300@gmail.com>
2023-01-22 22:52:56 +0100

    config

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter >
+  <BrowserRouter basename='/contact'>
     <Provider store={store}>
       <App />
     </Provider>
