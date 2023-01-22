c9ed2172cb890aa8c76d67cdcc373fab0f129d39
Ayman Chabat <aymanechabat300@gmail.com>
2023-01-22 23:22:02 +0100

    setup gh-pages

---
+++ build/precache-manifest.f220847ed4252f58aec1dea304436e8b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "828e31fa442b97ee84cb",
+    "url": "/Contact/static/css/main.7ff91d9b.chunk.css"
+  },
+  {
+    "revision": "828e31fa442b97ee84cb",
+    "url": "/Contact/static/js/main.262ad116.chunk.js"
+  },
+  {
+    "revision": "d1cb789313b2f8f61393",
+    "url": "/Contact/static/js/runtime~main.c58703e6.js"
+  },
+  {
+    "revision": "32611f9ff0509dc0e978",
+    "url": "/Contact/static/css/2.feab3d81.chunk.css"
+  },
+  {
+    "revision": "32611f9ff0509dc0e978",
+    "url": "/Contact/static/js/2.da8ad7c1.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Contact/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Contact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Contact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Contact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Contact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f3409755f25c765283a6695b253fa32b",
+    "url": "/Contact/index.html"
+  }
+];
